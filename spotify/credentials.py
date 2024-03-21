7380f654067cbd7e879c2da1154ca2e8016ab851
Marcos Mantovani <marcospiderplays@gmail.com>
2024-03-21 18:17:07 -0300

    wip

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = 'e6aaec608a68466aafa691f3dc628b93'
 CLIENT_SECRET = '70e57e56f5c7409ba93a58552585ccd9'
-REDIRECT_URI = "ShareSpotifyMusic.pythonanywhere.com/spotify/redirect"
+REDIRECT_URI = "https://ShareSpotifyMusic.pythonanywhere.com/spotify/redirect"
