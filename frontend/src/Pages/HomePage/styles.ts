a6251afefabfab35699c48b143d8f4336167fdd5
Marcos Mantovani <marcospiderplays@gmail.com>
2024-03-21 19:27:01 -0300

    feat: completed deploy on pythonanywhere

--- frontend/src/Pages/HomePage/styles.ts
+++ frontend/src/Pages/HomePage/styles.ts
@@ -13,3 +13,12 @@ export const StyledCard = styled.div`
     box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.7);
   }
 `
+
+export const Obs = styled.div`
+  .spotify-premium {
+    position: absolute;
+    bottom: 24px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`
